ac8f8a3f391b24a93c6cdeb77ae1b77ca8083996
Bakshi Iskandarov <bifo92@gmail.com>
2020-06-18 20:43:08 +0400

    * updated dependencies * added jsconfig for better editor

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
-import postcss from 'postcss';
+import postcss from "postcss";
 
-import azerbaijaniProperties from './properties';
-import azerbaijaniValues from './values';
+import azerbaijaniProperties from "./properties";
+import azerbaijaniValues from "./values";
 
-export default postcss.plugin('postcss-aze-stylesheets', () => {
+export default postcss.plugin("postcss-aze-stylesheets", () => {
   return (css) => {
     css.walkDecls((decl) => {
-      Object.keys(azerbaijaniProperties).forEach((property) => {
-        const azerbaijaniProperty = azerbaijaniProperties[property];
-        if (azerbaijaniProperty) {
-          decl.prop === azerbaijaniProperty && (decl.prop = property);
-        }
-
-        if (decl.value.indexOf('!vacib') >= 0) {
-          decl.value = decl.value.replace(/\s*!vacib\s*/, '');
-          decl.important = true;
-        }
-      });
-
-      Object.keys(azerbaijaniValues).forEach((value) => {
-        const azerbaijaniValue = azerbaijaniValues[value];
-        if (azerbaijaniValue) {
-          decl.value = decl.value.replace(new RegExp(azerbaijaniValue), value);
-        }
-      });
+      const englishProp = azerbaijaniProperties[decl.prop];
+      if (englishProp) decl.prop = englishProp;
+      if (decl.value.indexOf("!vacib") >= 0) {
+        decl.value = decl.value.replace(/\s*!vacib\s*/, "");
+        decl.important = true;
+      }
+      const englishVal = azerbaijaniValues[decl.value];
+      if (englishVal) {
+        decl.value = englishVal;
+      }
     });
   };
 });
